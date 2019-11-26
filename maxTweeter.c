fde4d9453fabd5b93ce3eafdc2790baba74a14d1
Harshita Kaushal <harshitakaushal@Harshitas-MacBook-Pro.local>
2019-11-25 21:01:23 -0800

    put into separate function

--- maxTweeter.c
+++ maxTweeter.c
@@ -25,52 +25,34 @@ int temp = 1000;
 //can declare this within main 
 struct USER_ARR GLOBAL_USERS[20000];
 
+int name_pos = 0;
 
-int main(int argc, char* argv[]){
+int find_row_pos(char *file_csv){
 
-// A valid input csv file has columns separated with “,”’s, but you cannot assume the location of the
-// tweeter column will be fixed to a particular location (like 8 for instance). A valid csv file will have
-// a header, and the tweeter column will be called “name”, and the items may or may not be
-// surrounded by quotes (“/”). You may also assume that the maximum line length of any valid csv
-// file will not be longer than 1024 characters, the length of the file will not exceed 20,000 lines.
-// Finally, for this assignment, a valid file will not have additional commas inside the tweet.
-// However, your program should not crash on any input, even invalid ones.
-
-        FILE *csv_file = fopen(argv[1], "r"); // "r" for read
-
-
+        FILE *csv_file = fopen(file_csv, "r"); // "r" for read
+        
+        //todo more error checking later on 
         if (!csv_file) {
                 printf("Can't open file\n");
                 return -1;
         }
-
         char buf[1024];
         int row_count = 0;
         int field_count = 0;
         int index_of_field = 0; 
-        int name_pos = 0;
         while (fgets(buf, 1024, csv_file)) {
                 field_count = 0;
                 row_count++;
-
-                // if (row_count == 1) {
-                // continue;
-                // }
-
                 char *field = strtok(buf, ",");
+                
+                // this is for the first row
+                //assuming that header is always row1
                 while (field) {
 
-                        // this is only for the first one
-                        //assuming that header is always row1
-
-                
-                printf("%d : %s\n", index_of_field ,field);
-                if (strcmp(field, "name") ==0){
-                       
-                        name_pos = index_of_field;
-                        printf("Name is at index %d\n", name_pos);
-                        break;
-
+                        printf("%d : %s\n", index_of_field ,field);
+                        if (strcmp(field, "name") ==0){
+                                name_pos = index_of_field;
+                                break;
                 }
                 index_of_field++;
 
@@ -85,8 +67,17 @@ int main(int argc, char* argv[]){
 
         fclose(csv_file);
 
-        return 0;
 
+
+}
+int main(int argc, char* argv[]){
+
+
+        char* file_name = argv[1];
+        find_row_pos(argv[1]);
+        printf("Name is at index %d\n", name_pos);
+
+        return 0;
 
 
 
