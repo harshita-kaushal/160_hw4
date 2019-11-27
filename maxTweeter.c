bb90bc7be4053a7c2d4ce35aaf1107dfc3fe0474
Sanjna Duvvur <sanjna.duvvur@gmail.com>
2019-11-26 22:15:41 -0800

    test all rows for correct commas

--- maxTweeter.c
+++ maxTweeter.c
@@ -8,8 +8,8 @@
 
 struct USER_ARR
 {
-        char twitter_username[MAX_LINE_LEN]; //clarify later
-        int count_of_tweets;
+  char twitter_username[MAX_LINE_LEN]; //clarify later
+  int count_of_tweets;
 };
 
 //can declare this within main
@@ -23,141 +23,149 @@ int global_header_comma_count = 0;
 int find_name_pos(char *file_csv)
 {
 
-        FILE *csv_file = fopen(file_csv, "r"); // "r" for read
+  FILE *csv_file = fopen(file_csv, "r"); // "r" for read
 
-        //todo more error checking later on
-        //checks if file exists
-        if (!csv_file)
-        {
-                printf("Can't open file\n");
-                return -1;
-        }
-        char buf[1024];
-        int row_count = 0;
-        int field_count = 0;
-        int index_of_field = 0;
+  //todo more error checking later on
+  //checks if file exists
+  if (!csv_file)
+  {
+    printf("Can't open file\n");
+    return -1;
+  }
+  char buf[1024];
+  int row_count = 0;
+  int field_count = 0;
+  int index_of_field = 0;
 
-        while (fgets(buf, 1024, csv_file))
-        {
-                field_count = 0;
-                row_count++;
-                char *field = strtok(buf, ",");
+  while (fgets(buf, 1024, csv_file))
+  {
+    field_count = 0;
+    row_count++;
+    char *field = strtok(buf, ",");
 
-                // this is for the first row
-                //assuming that header is always row1
-                while (field)
-                {
+    // this is for the first row
+    //assuming that header is always row1
+    while (field)
+    {
 
-                        // printf("%d : %s\n", index_of_field ,field);
-                        if (strcmp(field, "name") == 0)
-                        {
-                                global_name_pos = index_of_field;
-                                break;
-                        }
-                        index_of_field++;
+      // printf("%d : %s\n", index_of_field ,field);
+      if (strcmp(field, "name") == 0)
+      {
+        global_name_pos = index_of_field;
+        break;
+      }
+      index_of_field++;
 
-                        field = strtok(NULL, ",");
+      field = strtok(NULL, ",");
 
-                        field_count++;
-                }
-                // printf("\n");
-                break;
-        }
-        fclose(csv_file);
-        return global_name_pos;
+      field_count++;
+    }
+    // printf("\n");
+    break;
+  }
+  fclose(csv_file);
+  return global_name_pos;
 }
 
 int processing_file(char *file_csv)
 {
 
-        FILE *csv_file = fopen(file_csv, "r"); // "r" for read
+  FILE *csv_file = fopen(file_csv, "r"); // "r" for read
 
-        if (!csv_file)
+  if (!csv_file)
+  {
+    printf("Can't open file\n");
+    return -1;
+  }
+
+  char buf[1024];
+  int row_count = 0;
+  int field_count = 0;
+
+  while (fgets(buf, 1024, csv_file))
+  {
+
+    field_count = 0;
+    row_count++;
+    int comma_count = 0;
+    int row_comma_count = 0;
+
+    int cur_char = 0;
+    while (cur_char < 1024)
+    {
+      //need to get global_comma_count
+      if (row_count == 1)
+      {
+        char cpy = buf[cur_char];
+        if (cpy == ',')
         {
-                printf("Can't open file\n");
-                return -1;
-        }
-        
-        char buf[1024];
-        int row_count = 0;
-        int field_count = 0;
+          cur_char += 1;
+          global_header_comma_count += 1;
+          printf("global_header_comma_count is %d\n", global_header_comma_count);
 
-        while (fgets(buf, 1024, csv_file))
-        {
-
-                field_count = 0;
-                row_count++;
-                int comma_count = 0;
-
-                int cur_char = 0;
-                while (cur_char < 1024)
-                {
-                        //need to get global_comma_count
-                        if (row_count == 1)
-                        {
-                                char cpy = buf[cur_char];
-                                if (cpy == ',')
-                                {
-                                        cur_char += 1;
-                                        global_header_comma_count += 1;
-                                        printf("global_header_comma_count is %d\n", global_header_comma_count);
-
-                                }
-
-                                else
-                                {
-                                        cur_char += 1;
-                                }
-                
-                        }
-                        else
-                        {
-                                char cpy = buf[cur_char];
-                                if (cpy == ',')
-                                {
-                                        // printf("cpy %c is %d\n", cpy, comma_count);
-                                        cur_char += 1;
-                                        comma_count += 1;
-                                        if (comma_count == 8)
-                                        {
-                                                // printf("comma count is %d\n", comma_count);
-                                                break;
-                                        }
-                                }
-                                else
-                                {
-                                        // printf("buf[%d] is %c\n",cur_char, cpy);
-                                        cur_char += 1;
-                                }
-                        }
-
-                        // }
-                }
-
-                
         }
 
-        return 0;
+        else
+        {
+          cur_char += 1;
+        }
+
+      }
+      else
+      {
+        char cpy = buf[cur_char];
+        if (cpy == ',')
+        {
+          cur_char += 1;
+          row_comma_count += 1;
+          printf("row comma count is %d\n", row_comma_count);
+
+          if(row_comma_count > global_header_comma_count){
+            printf("Invalid Input Format\n");
+            printf("global_header_comma_count is %d\n", global_header_comma_count);
+            return -1;
+          }
+
+        }
+
+        else
+        {
+          cur_char += 1;
+        }
+      }
+
+
+    }
+
+    if(row_comma_count < global_header_comma_count)
+    {
+        printf("Invalid Input Format\n");
+        printf("global_header_comma_count is %d\n", global_header_comma_count);
+        return -1;
+    }
+  }
+
+  return 0;
 }
 
 
-        int main(int argc, char *argv[])
-        {
+int main(int argc, char *argv[])
+{
 
-                // char* file_name = argv[1];
+  // char* file_name = argv[1];
 
-                int res_find_row = find_name_pos(argv[1]);
+  int res_find_row = find_name_pos(argv[1]);
 
-                int process_rest_file = processing_file(argv[1]);
-                // printf("processing is taking a long time");
-                //couldn't open file
-                if (res_find_row == -1 || process_rest_file == -1)
-                {
-                        printf("Invalid Input Format");
-                        return 0;
-                }
+  int process_rest_file = processing_file(argv[1]);
+  // printf("processing is taking a long time");
+  //couldn't open file
+  if (res_find_row == -1 || process_rest_file == -1)
+  {
+    printf("Invalid Input Format");
+    return 0;
+  }
 
-                // printf("Name is at index %d\n", global_name_pos);
+  // printf("Name is at index %d\n", global_name_pos);
 
-                return 0;
-        }
+  return 0;
+}
